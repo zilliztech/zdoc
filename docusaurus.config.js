d5fc643d3fedff07cf0e9c2cf8ff3aeb5118e4e3
AnthonyTsu1984 <115786031+AnthonyTsu1984@users.noreply.github.com>
2024-02-20 16:30:18 +0800

    update docs
    Signed-off-by: AnthonyTsu1984 <115786031+AnthonyTsu1984@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -366,7 +366,8 @@ const config = {
       tagName: 'link',
       attributes: {
         rel:'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap',
+        //href: 'https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap',
+        href: 'https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'
       }
     }
   ],
