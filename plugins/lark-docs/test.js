cc6953734b9333dc6d7396f56c2aba5b6a654d56
AnthonyTsu1984 <115786031+AnthonyTsu1984@users.noreply.github.com>
2024-02-29 16:05:03 +0800

    update docs
    Signed-off-by: AnthonyTsu1984 <115786031+AnthonyTsu1984@users.noreply.github.com>

--- plugins/lark-docs/test.js
+++
@@ -1,7 +0,0 @@
-const larkUtils = require('./larkUtils');
-
-const utils = new larkUtils();
-const outputDir = 'milvus/reference/python/docs/MilvusClient/MilvusClient-Client'
-const docsourceDir = ''
-
-utils.postprocess_for_milvus(outputDir, docsourceDir)
