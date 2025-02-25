964814be8cef5daf4ef93b61ac3001228c176fb9
AnthonyTsu1984 <115786031+AnthonyTsu1984@users.noreply.github.com>
2025-02-25 19:00:19 +0800

    update docs
    Signed-off-by: AnthonyTsu1984 <115786031+AnthonyTsu1984@users.noreply.github.com>

--- plugins/apifox-docs/refGen.js
+++ plugins/apifox-docs/refGen.js
@@ -104,7 +104,7 @@ class refGen {
       const description = descriptions.filter(x => x.name === slug)[0].description
       const position = specifications.tags.map(x => x.name).indexOf(specifications.tags[group].name)
       var beta_tag = version === 'v2' ? 'FALSE' : 'NEAR DEPRECATE'
-      beta_tag = page_slug.includes('invoice') || page_slug.includes('usage') ? 'PUBLIC' : beta_tag
+      beta_tag = slug.includes('invoice') || slug.includes('usage') ? 'PUBLIC' : beta_tag
       const t = template.render({
         group_name,
         position,
