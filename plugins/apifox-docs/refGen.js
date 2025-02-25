342339e056080ee472068f468920d302e4d02ca2
AnthonyTsu1984 <115786031+AnthonyTsu1984@users.noreply.github.com>
2025-02-25 17:16:21 +0800

    update docs
    Signed-off-by: AnthonyTsu1984 <115786031+AnthonyTsu1984@users.noreply.github.com>

--- plugins/apifox-docs/refGen.js
+++ plugins/apifox-docs/refGen.js
@@ -59,7 +59,7 @@ class refGen {
         }
         var page_slug = (this.get_slug(page_title, target)) + slug_suffix
         var beta_tag = version === 'v2' ? 'FALSE' : 'NEAR DEPRECATE'
-        beta_tag = slug.includes('invoice') || slug.includes('usage') ? 'PUBLIC' : 'FALSE'
+        beta_tag = slug.includes('invoice') || slug.includes('usage') ? 'PUBLIC' : beta_tag
         const page_method = method.toLowerCase()
         const specs = JSON.stringify(specification)
 
@@ -104,7 +104,7 @@ class refGen {
       const description = descriptions.filter(x => x.name === slug)[0].description
       const position = specifications.tags.map(x => x.name).indexOf(specifications.tags[group].name)
       var beta_tag = version === 'v2' ? 'FALSE' : 'NEAR DEPRECATE'
-      beta_tag = page_slug.includes('invoice') || page_slug.includes('usage') ? 'PUBLIC' : 'FALSE'
+      beta_tag = page_slug.includes('invoice') || page_slug.includes('usage') ? 'PUBLIC' : beta_tag
       const t = template.render({
         group_name,
         position,
