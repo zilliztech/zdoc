48c178d34fe1ed89358cdc54955d50ba24244414
AnthonyTsu1984 <115786031+AnthonyTsu1984@users.noreply.github.com>
2025-05-21 18:10:31 +0800

    implement AI bot phase II
    Signed-off-by: AnthonyTsu1984 <115786031+AnthonyTsu1984@users.noreply.github.com>

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -9,34 +9,13 @@ export default function Banner({ bannerText, bannerLinkText }) {
     const [isOpen, setIsOpen] = useState(false);
     const { siteConfig } = useDocusaurusContext();
 
-    const { apiKey, integrationId, organizationId} = siteConfig.themeConfig.inkeepConfig.baseSettings;
-
     const handleOpenChange = useCallback((isOpen) => {
         setIsOpen(isOpen);
     }, []);
 
-    const config = {
-        baseSettings: {
-            apiKey,
-            integrationId,
-            organizationId,
-            organizationDisplayName: 'Zilliz Cloud',
-            primaryBrandColor: '#175fff'
-        },
-        aiChatSettings: {
-            aiAssistantName: 'Zilliz',
-            chatSubjectName: 'Zilliz Cloud',
-            placeholder: 'How do I get started?',
-            introMessage: "Hi!\nI'm an AI assistan,t trained on documentation, help articles, and other content.\nAsk me anything about",
-            isShareButtonVisible: true,
-            isCopyChatButtonVisible: true,
-            exampleQuestions: [
-                "How do I create and connect to a cluster in Zilliz Cloud?",
-                "How can I optimize vector search performance for large datasets?",
-                "What are the differences between Serverless and Dedicated clusters?"
-            ]
-        }
-    }
+    const config = siteConfig.plugins.find( plugin => {
+        return plugin[0] === '@inkeep/cxkit-docusaurus';
+    })[1].SearchBar;
 
     config.modalSettings = {
         isOpen,
