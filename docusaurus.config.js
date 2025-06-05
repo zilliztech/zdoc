74020ca0bd45d9176bf02375e1181eeef38887be
AnthonyTsu1984 <115786031+AnthonyTsu1984@users.noreply.github.com>
2025-06-05 13:18:57 +0800

    update docs
    Signed-off-by: AnthonyTsu1984 <115786031+AnthonyTsu1984@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -868,7 +868,15 @@ const config = {
     }
   ],
   scripts: [],
-  stylesheets: [],
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
 };
 
 module.exports = config;
