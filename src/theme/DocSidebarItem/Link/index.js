20d741f2788c95082ef78403a6dda97da5148aa1
AnthonyTsu1984 <wenjia.tu@zilliz.com>
2025-10-22 18:07:32 +0800

    update docs
    Signed-off-by: AnthonyTsu1984 <wenjia.tu@zilliz.com>

--- src/theme/DocSidebarItem/Link/index.js
+++ src/theme/DocSidebarItem/Link/index.js
@@ -23,6 +23,9 @@ const Badge = ({ label }) => {
       case "DELETE":
           color = "#b91926";
           break;
+      case "PATCH":
+          color = "#ff9900";
+          break;
       default:
           color = "#000000";
   }  
